041baf5170f0a426bc0869143f545a53fc438505
ysheep0906 <wlq94814@naver.com>
2025-12-11 11:11:42 +0900

    Fix opensearch

--- src/main/java/com/example/facticle/FacticleApplication.java
+++ src/main/java/com/example/facticle/FacticleApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.TimeZone;
